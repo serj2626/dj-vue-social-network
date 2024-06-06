18594ff93c0e1685435660dfe17a2d80a37b109f
serj2626 <serj2626@mail.ru>
2024-06-07 01:45:01 +0300

    added jwt tokens in views

--- backend/account/api.py
+++ backend/account/api.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import (
+    api_view, authentication_classes, permission_classes
+)
 from .forms import SignupForm
 
 
