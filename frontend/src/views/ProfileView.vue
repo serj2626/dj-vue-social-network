2aaf2c603866d4871301bed1160c1922b05973b3
serj2626 <serj2626@mail.ru>
2024-08-23 02:20:01 +0300

    add watchEffect in profile page

--- frontend/src/views/ProfileView.vue
+++ frontend/src/views/ProfileView.vue
@@ -5,106 +5,127 @@ import Trends from "../components/Trends.vue";
 import FeedItem from "../components/FeedItem.vue";
 import { useToast } from "vue-toastification";
 import { useUserStore } from "@/stores/user";
-import { onMounted, reactive, ref } from "vue";
+import { onBeforeUpdate, onMounted, onUpdated, reactive, ref, watchEffect } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
-const { id } = route.params;
 const toast = useToast();
 
 const userStore = useUserStore();
 
 const posts = ref([]);
 const user = reactive({
-    id: "",
-    name: "",
-    email: "",
+  id: "",
+  name: "",
+  email: "",
 });
 
 const body = ref("");
 
-
-
 async function getFeed() {
-    try {
-        const { data } = await axios.get(`/api/posts/profile/${id}/`);
-        posts.value = data.posts;
+  try {
+    const { data } = await axios.get(`/api/posts/profile/${route.params.id}/`);
+    posts.value = data.posts;
 
-        user.id = data.user.id;
-        user.name = data.user.name;
-        user.email = data.user.email;
-        console.log(user);
-    } catch {
-        toast.error("Произошла ошибка при загрузке постов");
-    }
+    user.id = data.user.id;
+    user.name = data.user.name;
+    user.email = data.user.email;
+    console.log(user);
+  } catch {
+    toast.error("Произошла ошибка при загрузке постов");
+  }
 }
 
 const submitForm = async () => {
-    if (body.value === "") {
-        toast.error("Форма не может быть пустой");
-        return
+  if (body.value === "") {
+    toast.error("Форма не может быть пустой");
+    return;
+  } else {
+    try {
+      await axios.post("/api/posts/", { body: body.value });
+      toast.success("Пост успешно создан");
+      body.value = "";
+      await getFeed();
+    } catch {
+      toast.error("Произошла ошибка при создании поста");
     }
-    else {
-        try {
-            await axios.post("/api/posts/", { body: body.value });
-            toast.success("Пост успешно создан");
-            body.value = "";
-            await getFeed();
-        } catch {
-            toast.error("Произошла ошибка при создании поста");
-        }
-    }
+  }
 };
 
-onMounted(() => {
-    getFeed();
+
+watchEffect(() => {
+    console.log("watchEffect");
+  getFeed();
 })
+
 </script>
 
 <template>
-    <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
-        <div class="main-left col-span-1">
-            <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
-                <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
+  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+    <div class="main-left col-span-1">
+      <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
+        <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
 
-                <p>
-                    <strong>{{ user.name }}</strong>
-                </p>
+        <p>
+          <strong>{{ user.name }}</strong>
+        </p>
 
-                <div class="mt-6 flex space-x-8 justify-around">
-                    <p class="text-xs text-gray-500">182 friends</p>
-                    <p class="text-xs text-gray-500">120 posts</p>
-                </div>
-            </div>
+        <div class="mt-6 flex space-x-8 justify-around">
+          <p class="text-xs text-gray-500">182 friends</p>
+          <p class="text-xs text-gray-500">120 posts</p>
         </div>
-
-        <div class="main-center col-span-2 space-y-4">
-            <div class="bg-white border border-gray-200 rounded-lg" v-if="userStore.user.id === id">
-                <form @submit.prevent="submitForm" method="post">
-                    <div class="p-4">
-                        <textarea v-model="body" class="p-4 w-full bg-gray-100 rounded-lg"
-                            placeholder="Что нового?"></textarea>
-                    </div>
-
-                    <div class="p-4 border-t border-gray-100 flex justify-between">
-                        <a href="#" class="inline-block py-4 px-6 bg-gray-600 text-white rounded-lg">Прикрепить
-                            изображение</a>
-
-                        <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">
-                            Отправить
-                        </button>
-                    </div>
-                </form>
-            </div>
-
-            <div class="p-4 bg-white border border-gray-200 rounded-lg" v-for="post in posts" :key="post.id">
-                <FeedItem :post="post" />
-            </div>
-        </div>
-
-        <div class="main-right col-span-1 space-y-4">
-            <PeopleYouMayKnow />
-            <Trends />
-        </div>
+      </div>
     </div>
+
+    <div class="main-center col-span-2 space-y-4">
+      <div
+        class="bg-white border border-gray-200 rounded-lg"
+        v-if="userStore.user.id === user.id"
+      >
+        <form @submit.prevent="submitForm" method="post">
+          <div class="p-4">
+            <textarea
+              v-model="body"
+              class="p-4 w-full bg-gray-100 rounded-lg"
+              placeholder="Что нового?"
+            ></textarea>
+          </div>
+
+          <div class="p-4 border-t border-gray-100 flex justify-between">
+            <a
+              href="#"
+              class="inline-block py-4 px-6 bg-gray-600 text-white rounded-lg"
+              >Прикрепить изображение</a
+            >
+
+            <button
+              class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg"
+            >
+              Отправить
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div v-if="posts.length > 0"
+        class="p-4 bg-white border border-gray-200 rounded-lg"
+        v-for="post in posts"
+        :key="post.id"
+      >
+        <FeedItem :post="post" />
+      </div>
+
+      <div
+        class="p-4 bg-white border border-gray-200 rounded-lg"
+        v-else
+      >
+        <p>Посты не найдены</p>
+      </div>
+    </div>
+
+    <div class="main-right col-span-1 space-y-4">
+      <PeopleYouMayKnow />
+      <Trends />
+    </div>
+  </div>
 </template>
