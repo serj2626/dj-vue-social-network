cb2430259c7d6811498996644b9bd69796e0c9e0
serj2626 <serj2626@mail.ru>
2024-08-23 01:20:15 +0300

    add new view by all posts profile user

--- backend/posts/api.py
+++ backend/posts/api.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-
+from drf_spectacular.utils import extend_schema
 from .models import Post
 from .serializers import PostSerializer
 
@@ -7,7 +7,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 
-class PostListViews(generics.ListCreateAPIView):
+@extend_schema(tags=["Posts"])
+class PostListView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [AllowAny]
@@ -17,12 +18,22 @@ class PostListViews(generics.ListCreateAPIView):
             data=self.request.data, context={'request': self.request})
         serializer.is_valid(raise_exception=True)
         serializer.save(author=self.request.user)
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=201)
+
+    @extend_schema(summary="Список постов")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(summary="Создание поста")
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 
-class PostListPerUser(generics.ListAPIView):
+@extend_schema(tags=["Posts"], summary="Список постов пользователя")
+class PostListProfileView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [AllowAny]
 
     def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+        uuid = self.kwargs['id']
+        return Post.objects.filter(author_id=uuid)
--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .api import PostListViews
+from .api import PostListView, PostListProfileView
 
 urlpatterns = [
-    path("", PostListViews.as_view(), name="post-list"),
+    path("", PostListView.as_view(), name="post-list"),
+    # path("create/", PostListView.as_view(), name="post-create"),
+    path("profile/<uuid:id>/", PostListProfileView.as_view(),
+         name="post-list-profile"),
 ]
