f5e4f8fb40794d4c3e3a91e15f54aefe4af2fa61
serj2626 <serj2626@mail.ru>
2024-08-23 23:58:02 +0300

    new class FriendshipRequestAdmin in admin panel

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, FriendshipRequest
 
 
 @admin.register(User)
@@ -8,3 +8,19 @@ class UserAdmin(admin.ModelAdmin):
     '''Admin View for User'''
 
     list_display = ('id', 'email', 'name', 'avatar', 'is_superuser' )
+
+
+@admin.register(FriendshipRequest)
+class FriendshipRequestAdmin(admin.ModelAdmin):
+    '''Admin View for FriendshipRequest)'''
+
+    list_display = ( 'created_for', 'created_at', 'created_by', 'status', )
+    # list_filter = ('',)
+    # inlines = [
+    #     Inline,
+    # ]
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    # search_fields = ('',)
+    # date_hierarchy = ''
+    # ordering = ('',)
