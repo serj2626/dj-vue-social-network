7a57308e487caccdd5b4f117fad20263c6ed8558
serj2626 <serj2626@mail.ru>
2024-08-23 23:43:54 +0300

    new prop user in card component

---
+++ frontend/src/components/UserCard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps({
+    user: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+<template>
+    <RouterLink :to="{ name: 'profile', params: { id: user.id } }">
+        <div class="user-card p-4 text-center bg-gray-100 rounded-lg">
+            <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
+
+            <p><strong>{{ user.name }}</strong></p>
+
+            <div class="mt-6 flex space-x-8 justify-around">
+                <p class="text-xs text-gray-500">182 friends</p>
+                <p class="text-xs text-gray-500">120 posts</p>
+            </div>
+        </div>
+    </RouterLink>
+</template>
+<style scoped>
+.user-card{
+    transition: all 0.2s ease-in-out;
+
+    &:hover{
+        transform: scale(1.02);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.18);
+
+    }
+}
+</style>
