f1f6c4d41ec25a0693c6b1f1558778a51fb51d17
serj2626 <serj2626@mail.ru>
2024-08-24 13:24:59 +0300

    update imports

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import User
+from .models import User, FriendshipRequest
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'name', 'email',)
+
+
+class FriendshipRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FriendshipRequest
+        fields = ('created_by', 'created_for', 'status')
