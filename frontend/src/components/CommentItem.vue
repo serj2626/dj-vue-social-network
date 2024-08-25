9742dc9f64ca47a1bc718d6d5f0169d8a5f1ab72
serj2626 <serj2626@mail.ru>
2024-08-26 00:14:29 +0300

    new component

---
+++ frontend/src/components/CommentItem.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+    comment: {
+        type: Object,
+        required: true
+    }
+})
+
+</script>
+<template>
+    <div class="mb-6 flex items-center justify-between">
+        <div class="flex items-center space-x-6">
+            <img alt="avatar"  class="w-[40px] rounded-full">
+            <p>
+                <strong>
+                    <RouterLink :to="{name: 'profile', params:{'id': comment.author.id}}">{{ comment.author.name }}</RouterLink>
+                </strong>
+            </p>
+        </div>
+
+        <p class="text-gray-600">{{ comment.created_at_formatted }} ago</p>
+    </div>
+
+    <p>{{ comment.body }}</p> 
+</template>
+<style scoped>
+ 
+</style>
