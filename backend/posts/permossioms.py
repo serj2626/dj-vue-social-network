88d64e21aafeadd02a5778ab06574242916a81ec
serj2626 <serj2626@mail.ru>
2024-08-25 14:13:52 +0300

    new permisson for post detail

---
+++ backend/posts/permossioms.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAdminOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return bool(request.user and request.user.is_staff)
+
+
+class IsAuthorOrReadOnly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
