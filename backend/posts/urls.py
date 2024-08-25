c7e0fc2c14823a0d6603692c456b9e99e08fc151
serj2626 <serj2626@mail.ru>
2024-08-26 00:13:36 +0300

    new url for create review

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .api import PostDetailView, PostListProfileView, PostListView, toggle_like
+from .api import PostDetailView, PostListProfileView, PostListView, toggle_like,post_create_comment
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post-list"),
     path("detail/<uuid:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("detail/<uuid:pk>/comment/create/", post_create_comment, name="post-create-comment"),
     path("detail/<uuid:pk>/like/", toggle_like, name="toggle-like-post"),
     path("profile/<uuid:pk>/", PostListProfileView.as_view(), name="post-list-profile"),
 ]
