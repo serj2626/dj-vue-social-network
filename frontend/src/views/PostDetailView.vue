1a8cc7baef4e1626a940ca50512c889e38ac7ce8
serj2626 <serj2626@mail.ru>
2024-08-26 00:33:39 +0300

    formatting code

--- frontend/src/views/PostDetailView.vue
+++ frontend/src/views/PostDetailView.vue
@@ -68,7 +68,7 @@ watchEffect(() => {
   <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
     <div class="main-center col-span-3 space-y-4">
       <div
-        class="p-4 bg-white border border-gray-200 rounded-lg"
+        
         v-if="post.id"
       >
         <PostCard :id="post.id" />
