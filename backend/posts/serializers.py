37c9a04e5d1eadc6f0f59498d65e30b07790dffb
serj2626 <serj2626@mail.ru>
2024-08-26 00:13:21 +0300

    new serializer by post detail

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -2,13 +2,39 @@ from rest_framework import serializers
 
 from account.serializers import UserSerializer
 
-from .models import Post
+from .models import Post, Comment
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ("id", "body", "author", "created_at_formatted")
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
-    likes_count = serializers.IntegerField(source="likes.count", read_only=True)
+    likes_count = serializers.IntegerField(
+        source="likes.count", read_only=True)
 
     class Meta:
         model = Post
-        fields = ("id", "body", "author", "created_at_formatted", "likes_count")
+        fields = ("id", "body", "author", "created_at_formatted",
+                  "likes_count", "comments_count")
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "body",
+            "author",
+            "created_at_formatted",
+            "likes_count",
+            "comments",
+            "comments_count",
+        )
