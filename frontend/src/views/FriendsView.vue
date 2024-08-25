0549d696a61444fd4be71bbd19d52d4c8f7fa9fe
serj2626 <serj2626@mail.ru>
2024-08-26 00:16:22 +0300

    update code

--- frontend/src/views/FriendsView.vue
+++ frontend/src/views/FriendsView.vue
@@ -19,126 +19,111 @@ const friends = ref([]);
 
 
 
-async function getFriends() {
-  try {
-    const { data } = await axios.get(`friends/send-request/${route.params.id}/`);
+// async function getFriends() {
+//   try {
+//     const { data } = await axios.get(`friends/send-request/${route.params.id}/`);
    
-    console.log(user);
-  } catch {
-    toast.error("Произошла ошибка при загрузке постов");
-  }
-}
+//     console.log(user);
+//   } catch {
+//     toast.error("Произошла ошибка при загрузке постов");
+//   }
+// }
 
-const sendFriendRequest = async () => {
-  toast.warning("Вы отправляете заявку в друзья");
-};
+// const sendFriendRequest = async () => {
+//   toast.warning("Вы отправляете заявку в друзья");
+// };
 
 
-watchEffect(() => {
-  console.log("watchEffect");
-  getFeed();
-});
+// watchEffect(() => {
+//   console.log("watchEffect");
+//   getFeed();
+// });
 </script>
 
+
 <template>
-  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
-    <div class="main-left col-span-1">
-      <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
-        <img :src="user.get_avatar" class="mb-6 rounded-full" />
-
-        <p>
-          <strong>{{ user.name }}</strong>
-        </p>
-
-        <div class="mt-6 flex space-x-8 justify-around">
-          <p class="text-xs text-gray-500">{{ user.friends_count }} friends</p>
-          <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="main-center col-span-2 space-y-4">
-      <div
-        class="p-4 bg-white border border-gray-200 rounded-lg"
-        v-if="friendshipRequests.length"
-      >
-        <h2 class="mb-6 text-xl">Friendship requests</h2>
-
-        <div
-          class="p-4 text-center bg-gray-100 rounded-lg"
-          v-for="friendshipRequest in friendshipRequests"
-          v-bind:key="friendshipRequest.id"
-        >
-          <img
-            :src="friendshipRequest.created_by.get_avatar"
-            class="mb-6 mx-auto rounded-full"
-          />
-
-          <p>
-            <strong>
-              sd
-            </strong>
-          </p>
-
-          <div class="mt-6 flex space-x-8 justify-around">
-            <p class="text-xs text-gray-500">
-             3 friends
-            </p>
-            <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
-          </div>
-
-          <div class="mt-6 space-x-4">
-            <button
-              class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg"
-         
-            >
-              Accept
-            </button>
-            <button
-              class="inline-block py-4 px-6 bg-red-600 text-white rounded-lg"
-     
-            >
-              Reject
-            </button>
-          </div>
-        </div>
-
-        <hr />
-      </div>
-
-      <div
-        class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-2 gap-4"
-        v-if="friends.length"
-      >
-        <div
-          class="p-4 text-center bg-gray-100 rounded-lg"
-          v-for="user in friends"
-          v-bind:key="user.id"
-        >
-          <img :src="user.get_avatar" class="mb-6 rounded-full" />
-
-          <p>
-            <strong>
-              <RouterLink :to="{ name: 'profile', params: { id: user.id } }">{{
-                user.name
-              }}</RouterLink>
-            </strong>
-          </p>
-
-          <div class="mt-6 flex space-x-8 justify-around">
-            <p class="text-xs text-gray-500">
-              {{ user.friends_count }} friends
-            </p>
-            <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="main-right col-span-1 space-y-4">
-      <PeopleYouMayKnow />
-
-      <Trends />
-    </div>
+  <div>
+    <h1>Friends Page</h1>
   </div>
 </template>
+<!-- <template>
+  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+      <div class="main-left col-span-1">
+          <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
+              <img :src="user.get_avatar" class="mb-6 rounded-full">
+              
+              <p><strong>{{ user.name }}</strong></p>
+
+              <div class="mt-6 flex space-x-8 justify-around">
+                  <p class="text-xs text-gray-500">{{ user.friends_count }} friends</p>
+                  <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
+              </div>
+          </div>
+      </div>
+
+      <div class="main-center col-span-2 space-y-4">
+          <div 
+              class="p-4 bg-white border border-gray-200 rounded-lg"
+              v-if="friendshipRequests.length"
+          >
+              <h2 class="mb-6 text-xl">Friendship requests</h2>
+
+              <div 
+                  class="p-4 text-center bg-gray-100 rounded-lg"
+                  v-for="friendshipRequest in friendshipRequests"
+                  v-bind:key="friendshipRequest.id"
+              >
+                  <img :src="friendshipRequest.created_by.get_avatar" class="mb-6 mx-auto rounded-full">
+              
+                  <p>
+                      <strong>
+                          <RouterLink :to="{name: 'profile', params:{'id': friendshipRequest.created_by.id}}">{{ friendshipRequest.created_by.name }}</RouterLink>
+                      </strong>
+                  </p>
+
+                  <div class="mt-6 flex space-x-8 justify-around">
+                      <p class="text-xs text-gray-500">{{ user.friends_count }} friends</p>
+                      <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
+                  </div>
+
+                  <div class="mt-6 space-x-4">
+                      <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg" @click="handleRequest('accepted', friendshipRequest.created_by.id)">Accept</button>
+                      <button class="inline-block py-4 px-6 bg-red-600 text-white rounded-lg" @click="handleRequest('rejected', friendshipRequest.created_by.id)">Reject</button>
+                  </div>
+              </div>
+
+              <hr>
+          </div>
+
+          <div 
+              class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-2 gap-4"
+              v-if="friends.length"
+          >
+              <div 
+                  class="p-4 text-center bg-gray-100 rounded-lg"
+                  v-for="user in friends"
+                  v-bind:key="user.id"
+              >
+                  <img :src="user.get_avatar" class="mb-6 rounded-full">
+              
+                  <p>
+                      <strong>
+                          <RouterLink :to="{name: 'profile', params:{'id': user.id}}">{{ user.name }}</RouterLink>
+                      </strong>
+                  </p>
+
+                  <div class="mt-6 flex space-x-8 justify-around">
+                      <p class="text-xs text-gray-500">{{ user.friends_count }} friends</p>
+                      <p class="text-xs text-gray-500">{{ user.posts_count }} posts</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <div class="main-right col-span-1 space-y-4">
+          <PeopleYouMayKnow />
+
+          <Trends />
+      </div>
+  </div>
+</template> -->
