603c2b7bff76efc27eef2558e1e74ba7e25aacfd
serj2626 <serj2626@mail.ru>
2024-08-26 00:53:44 +0300

    new admin classes in app chat

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import ConversationMessage, Conversation
 
-# Register your models here.
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    '''Admin View for Conversation'''
+
+    list_display = ('id', 'created_at', 'modified_at', )
+
+
+@admin.register(ConversationMessage)
+class ConversationMessageAdmin(admin.ModelAdmin):
+    '''Admin View for ConversationMessage'''
+
+    list_display = ('id', 'conversation', 'body', 'sent_to', 'created_at', 'created_by', )
+    # list_filter = ('',)
+    # inlines = [
+    #     Inline,
+    # ]
+    # raw_id_fields = ('',)
+    # readonly_fields = ('',)
+    # search_fields = ('',)
+    # date_hierarchy = ''
+    # ordering = ('',)
