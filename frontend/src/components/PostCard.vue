a0f750e14711ea460c23d28983871d5ca3c9e8f7
serj2626 <serj2626@mail.ru>
2024-08-25 15:47:58 +0300

    add emit like

--- frontend/src/components/PostCard.vue
+++ frontend/src/components/PostCard.vue
@@ -1,27 +1,67 @@
 <script setup>
+import axios from 'axios';
+import { onMounted, onUpdated, reactive, ref, watchEffect } from 'vue';
+import { useToast } from 'vue-toastification';
 
-defineProps({
-  post: {
-    type: Object,
+const props = defineProps({
+  id: {
+    type: [Number, String],
     required: true,
   },
 });
 
+const emit = defineEmits(['like']);
 
+const isLiked = ref(false);
+const toast = useToast();
+const post = reactive({
+  id: '',
+  body: '',
+  created_at_formatted: '',
+  likes_count: 0,
+});
+const author = reactive({
+  name: '',
+  id: '',
+  email: '',
+})
+
+const getPost = async () => {
+  try {
+    const { data } = await axios.get(`/api/posts/detail/${props.id}/`);
+    author.name = data.post.author.name;
+    author.id = data.post.author.id;
+    author.email = data.post.author.email;
+
+    post.id = data.post.id;
+    post.body = data.post.body;
+    post.created_at_formatted = data.post.created_at_formatted;
+    post.likes_count = data.post.likes_count;
+    isLiked.value = data.is_liked;
+  } catch (e) {
+    toast.error("Произошла ошибка при загрузке поста");
+  }
+}
+
+
+
+const togleLike = (id) =>{
+  emit('like', id);
+}
+watchEffect(() => {
+  getPost();
+})
 </script>
 <template>
   <div class="post p-4 bg-white border border-gray-200 rounded-lg">
     <div class="mb-6 flex items-center justify-between">
       <div class="flex items-center space-x-6">
-        <img
-          src="https://i.pravatar.cc/300?img=70"
-          class="w-[40px] rounded-full"
-        />
-        <RouterLink :to="{ name: 'profile', params: { id: post.author.id } }">
-          <p class="hover:underline">
-            <strong>{{ post.author.name }}</strong>
-          </p>
-        </RouterLink>
+        <img src="https://i.pravatar.cc/300?img=70" class="w-[40px] rounded-full" />
+
+        <p class="hover:underline">
+          <strong>{{ author.name }}</strong>
+        </p>
+
       </div>
 
       <p class="text-gray-600">{{ post.created_at_formatted }} назад</p>
@@ -32,41 +72,25 @@ defineProps({
     <div class="my-6 flex justify-between">
       <div class="flex space-x-6">
         <div class="flex items-center space-x-2">
-          <svg
-            @click="$emit('like', post.id)"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 like-svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-            ></path>
+          <svg 
+          @click="togleLike(post.id)"
+          :class="{ 'active': isLiked, 'like-svg-active': isLiked }"
+          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-6 like-svg">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z">
+            </path>
           </svg>
 
-          <span class="text-gray-500 text-xs"
-            >{{ post.likes_count }} лайков</span
-          >
+          <span class="text-gray-500 text-xs">{{ post.likes_count }} лайков</span>
         </div>
 
         <div class="flex items-center space-x-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-            ></path>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z">
+            </path>
           </svg>
 
           <span class="text-gray-500 text-xs">0 комментариев</span>
@@ -74,19 +98,11 @@ defineProps({
       </div>
 
       <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-          ></path>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z">
+          </path>
         </svg>
       </div>
     </div>
@@ -103,8 +119,18 @@ defineProps({
   }
 }
 
-/* .active {
+.active {
   fill: red;
   stroke: red;
-}  */
+}
+
+.like-svg-active {
+  cursor: pointer;
+  transition: all 0.1s ease-in-out;
+
+  &:hover {
+    fill: rgb(255, 253, 253);
+    stroke: rgb(73, 67, 67);
+  }
+}
 </style>
