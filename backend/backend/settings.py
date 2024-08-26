b61567bca4ef904d64f61fbbac3c8c7d4998315c
serj2626 <serj2626@mail.ru>
2024-08-26 22:02:23 +0300

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
 CORS_ALLOWED_ORIGINS = [
