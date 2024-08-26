ee43a20ca40867408925ff0e4acf33eba69595fd
serj2626 <serj2626@mail.ru>
2024-08-26 13:50:25 +0300

    updated ConversationAdmin

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,19 +1,31 @@
 from django.contrib import admin
-from .models import ConversationMessage, Conversation
+
+from .models import Conversation, ConversationMessage
 
 
 @admin.register(Conversation)
 class ConversationAdmin(admin.ModelAdmin):
-    '''Admin View for Conversation'''
+    """Admin View for Conversation"""
 
-    list_display = ('id', 'created_at', 'modified_at', )
+    list_display = (
+        "id",
+        "created_at",
+        "modified_at",
+    )
 
 
 @admin.register(ConversationMessage)
 class ConversationMessageAdmin(admin.ModelAdmin):
-    '''Admin View for ConversationMessage'''
+    """Admin View for ConversationMessage"""
 
-    list_display = ('id', 'conversation', 'body', 'sent_to', 'created_at', 'created_by', )
+    list_display = (
+        "id",
+        "conversation",
+        "body",
+        "sent_to",
+        "created_at",
+        "created_by",
+    )
     # list_filter = ('',)
     # inlines = [
     #     Inline,
