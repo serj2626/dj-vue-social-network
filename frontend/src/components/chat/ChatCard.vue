2b669dabf502b9d324efb304979b8cffe4fa2b51
serj2626 <serj2626@mail.ru>
2024-08-26 22:07:05 +0300

    new component ChatCard

---
+++ frontend/src/components/chat/ChatCard.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    id: {
+        type: [Number, String],
+        required: true
+    }
+})
+</script>
+<template>
+    <div>
+
+    </div>
+</template>
+<style scoped></style>
