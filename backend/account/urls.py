3c8febc3d2dbde17e7397c5a4c9bfb23f2d86869
serj2626 <serj2626@mail.ru>
2024-08-26 22:01:53 +0300

    update account app: new urls

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .api import me, send_friendship_request, signup
+from .api import me, send_friendship_request, friends, RegisterView
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("signup/", signup, name="signup"),
+    path("register/", RegisterView.as_view(), name="register"),
     path("me/", me, name="me"),
+    path("friends/", friends, name="friends-list"),
     path(
         "friends/send-request/<uuid:id>",
         send_friendship_request,
         name="friends_send_request",
     ),
+
 ]
