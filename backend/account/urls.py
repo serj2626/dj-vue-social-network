1e37c810103f61101f9ee952609978b3523a58f8
serj2626 <serj2626@mail.ru>
2024-09-02 01:21:20 +0300

    new urls: subscribers and  subscriptions

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .api import me, send_friendship_request, friends, RegisterView
+from .api import RegisterView, friends, me, send_friendship_request, subscribers, subscriptions
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", RegisterView.as_view(), name="register"),
     path("me/", me, name="me"),
-    path("friends/", friends, name="friends-list"),
+    path("friends/<uuid:pk>", friends, name="friends-list"),
+    path("subscribers/<uuid:pk>", subscribers, name="subscribers_list"),
+    path("subscriptions/<uuid:pk>", subscriptions, name="subscriptions_list"),
     path(
         "friends/send-request/<uuid:id>",
         send_friendship_request,
         name="friends_send_request",
     ),
-
 ]
