c2543800a7497a5415f7c84d8656917d0b5b7ba1
serj2626 <serj2626@mail.ru>
2024-09-02 01:24:58 +0300

    add imports bu new global components

--- frontend/src/components/global/index.js
+++ frontend/src/components/global/index.js
@@ -1,11 +1,15 @@
 import UIButton from "./UI/UIButton.vue";
 import Trends from "../Trends.vue";
 import PeopleYouMayKnow from "../PeopleYouMayKnow.vue";
+import UIAcceptButton from "./UI/UIAcceptButton.vue";
+import UIRejectButton from "./UI/UIRejectButton.vue";
 
 const components = [
     { name: "UIButton", component: UIButton },
     { name: "Trends", component: Trends },
     { name: "PeopleYouMayKnow", component: PeopleYouMayKnow },
+    { name: "UIAcceptButton", component: UIAcceptButton },
+    { name: "UIRejectButton", component: UIRejectButton },
 ];
 
 
