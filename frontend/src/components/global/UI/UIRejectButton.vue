d50def70f829f85cc26f50a6e9ab233bb2d978c2
serj2626 <serj2626@mail.ru>
2024-09-02 01:25:28 +0300

    new global button reject

---
+++ frontend/src/components/global/UI/UIRejectButton.vue
@@ -0,0 +1,14 @@
+<script setup >
+defineProps({
+    text: {
+        type: [String, Number],
+        required: true
+    }
+})
+</script>
+<template>
+    <button class="inline-block py-4 px-6 bg-red-600 text-white rounded-lg hover:bg-red-700">
+      {{ text }}
+    </button>
+</template>
+<style scoped></style>
