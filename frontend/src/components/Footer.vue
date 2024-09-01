a1f4e49b0d5fc208b66b21617c6b62d4fbd3b188
serj2626 <serj2626@mail.ru>
2024-09-02 01:23:21 +0300

    create footer component

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div class="flex items-center justify-around">
+        <div class="flex flex-col">
+             <a href="">Boytsov S.V.</a>
+             <p class="text-gray-500">© 2024. Санкт Петербург.</p>
+        </div>
+           
+        <div class="flex items-center  gap-4 py-8">
+            <i class="fa-brands fa-vk fa-xl" style="color: #028af2;"></i>
+            <i class="fa-brands fa-instagram fa-xl" style="color: #ff8f0f;"></i>
+            <i class="fa-brands fa-telegram fa-xl" style="color: #005eff;"></i>
+            <i class="fa-brands fa-github fa-xl" style="color: #000000;"></i>
+        </div>
+    </div>
+
+</template>
+<style scoped></style>
