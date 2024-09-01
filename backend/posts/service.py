18717b6bebf465eedbb4e78b7e92bb8237f6aed4
serj2626 <serj2626@mail.ru>
2024-09-02 01:23:00 +0300

    new service file

--- backend/posts/service.py
+++ backend/posts/service.py
@@ -1,2 +1,26 @@
+from account.models import FriendshipRequest
+
+
 def get_path_for_post_image(instance, filename):
     return f"post_attachments/{instance.author}/{filename}"
+
+
+def get_status(self, user):
+    status = ""
+
+    if user in self.request.user.friends.all():
+        status = "Ваш друг"
+    elif self.request.user in user.friends.all():
+        status = "Ваш друг"
+    elif FriendshipRequest.objects.filter(
+        created_by=self.request.user, created_for=user
+    ):
+        status = "Вы подписаны"
+    elif FriendshipRequest.objects.filter(
+        created_by=user, created_for=self.request.user
+    ):
+        status = "Ваш подписчик"
+    else:
+        status = "Отправить заявку"
+
+    return status
