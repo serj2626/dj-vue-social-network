6b75895dbe2413e50cd6c27d8816f96315fda922
serj2626 <serj2626@mail.ru>
2024-09-02 01:25:16 +0300

    new global component: accept btn

---
+++ frontend/src/components/global/UI/UIAcceptButton.vue
@@ -0,0 +1,14 @@
+<script setup >
+defineProps({
+    text: {
+        type: [String, Number],
+        required: true
+    }
+})
+</script>
+<template>
+    <button class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg hover:bg-purple-700">
+      {{ text }}
+    </button>
+</template>
+<style scoped></style>
