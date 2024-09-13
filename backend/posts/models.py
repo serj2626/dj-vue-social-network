f2e85d5371f3e10d4b2308ec67b17c5cc184525a
serj2626 <serj2626@mail.ru>
2024-09-13 14:48:18 +0300

    update field id in model PostAttachment

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,5 +1,3 @@
-import uuid
-
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.timesince import timesince
@@ -14,7 +12,6 @@ class PostAttachment(models.Model):
     Модель Прикрепленного файла
     """
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     image = models.ImageField(
         verbose_name="картинка", upload_to=get_path_for_post_image
     )
@@ -30,51 +27,6 @@ class PostAttachment(models.Model):
         verbose_name_plural = "Прикрепленные файлы"
 
 
-class Like(models.Model):
-    """
-    Модель Лайка
-    """
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_by = models.ForeignKey(
-        User, related_name="likes", on_delete=models.CASCADE, verbose_name="автор"
-    )
-    created_at = models.DateTimeField(verbose_name="дата создания", auto_now_add=True)
-
-    class Meta:
-        verbose_name = "Лайк"
-        verbose_name_plural = "Лайки"
-        ordering = ["-created_at"]
-
-    def created_at_formatted(self):
-        return timesince(self.created_at)
-
-    def __str__(self):
-        return f"Лайк от {self.created_by}"
-
-
-class Comment(models.Model):
-    """
-    Модель Комментария
-    """
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    body = models.TextField(blank=True, null=True, verbose_name="текст комментария")
-    author = models.ForeignKey(
-        User, related_name="comments", on_delete=models.CASCADE, verbose_name="автор"
-    )
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="дата создания")
-
-    def __str__(self):
-        return f"Комментарий от {self.author}"
-
-    class Meta:
-        ordering = ("-created_at",)
-        verbose_name = "Комментарий"
-        verbose_name_plural = "Комментарии"
-
-    def created_at_formatted(self):
-        return timesince(self.created_at)
 
 
 class Post(models.Model):
@@ -82,22 +34,16 @@ class Post(models.Model):
     Модель Поста
     """
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     body = models.TextField(verbose_name="текст поста", blank=True, null=True)
 
     attachments = models.ManyToManyField(
         PostAttachment, blank=True, verbose_name="прикрепленные файлы"
     )
 
-    likes = models.ManyToManyField(Like, blank=True, verbose_name="лайки")
-    likes_count = models.IntegerField(verbose_name="количество лайков", default=0)
+    likes = models.ManyToManyField(User, blank=True, verbose_name="лайки")
 
-    comments = models.ManyToManyField(Comment, blank=True, verbose_name="комментарии")
-    comments_count = models.IntegerField(
-        default=0, verbose_name="количество комментариев"
-    )
-
-    created_at = models.DateTimeField(verbose_name="дата создания", auto_now_add=True)
+    created_at = models.DateTimeField(
+        verbose_name="дата создания", auto_now_add=True)
     author = models.ForeignKey(
         User, related_name="posts", on_delete=models.CASCADE, verbose_name="автор"
     )
@@ -112,3 +58,31 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post {self.body[:10]}... by {self.author}"
+
+
+class Comment(models.Model):
+    """
+    Модель Комментария
+    """
+
+    body = models.TextField(blank=True, null=True,
+                            verbose_name="текст комментария")
+    author = models.ForeignKey(
+        User, related_name="user_comments", on_delete=models.CASCADE, verbose_name="автор"
+    )
+    post = models.ForeignKey(
+        Post, related_name="post_comments", on_delete=models.CASCADE, verbose_name="пост"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="дата создания")
+
+    def __str__(self):
+        return f"Комментарий от {self.author}"
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+    def created_at_formatted(self):
+        return timesince(self.created_at)
