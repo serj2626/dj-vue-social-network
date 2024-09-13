5042f3daed67d28da1e5ac4590ac77e2f4e6f9de
serj2626 <serj2626@mail.ru>
2024-09-13 14:52:18 +0300

    new css style for footer links

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -3,18 +3,23 @@
 </script>
 <template>
     <div class="flex items-center justify-around">
-        <div class="flex flex-col">
-             <a href="">Boytsov S.V.</a>
-             <p class="text-gray-500">© 2024. Санкт Петербург.</p>
-        </div>
-           
+            <p class="">© 2024 Санкт Петербург.</p>
+
         <div class="flex items-center  gap-4 py-8">
-            <i class="fa-brands fa-vk fa-xl" style="color: #028af2;"></i>
-            <i class="fa-brands fa-instagram fa-xl" style="color: #ff8f0f;"></i>
-            <i class="fa-brands fa-telegram fa-xl" style="color: #005eff;"></i>
-            <i class="fa-brands fa-github fa-xl" style="color: #000000;"></i>
+            <i class="fa-brands fa-vk fa-xl" title="vk" style="color: #028af2;"></i>
+            <i class="fa-brands text-red-600 fa-instagram fa-xl" title="instagram"></i>
+            <i class="fa-brands fa-telegram fa-xl" title="telegram" style="color: #005eff;"></i>
+            <i class="fa-brands fa-github fa-xl" title="github" style="color: #000000;"></i>
         </div>
     </div>
 
 </template>
-<style scoped></style>
+<style scoped>
+.fa-brands {
+    transition: scale .3s ease;
+
+    &:hover {
+        scale: 1.2;
+    }
+}
+</style>
